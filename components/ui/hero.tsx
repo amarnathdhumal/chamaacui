1233ead0c8ed9e0daecb2b5f38fa9f9f9ea6c50b
amarnath666 <amarnathdhumal2001@gmail.com>
2025-09-15 23:19:01 +0530

    added vercel analytics

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -11,7 +11,8 @@ const Hero = () => {
   return (
     <main className="relative h-screen w-full bg-black">
       {/* Background Image */}
-      <div className="absolute inset-0">
+      
+      <div className="absolute inset-0 ">
         <Image
           src="/images/hero.svg"
           alt="Hero"
@@ -35,7 +36,7 @@ const Hero = () => {
       </motion.div>
 
       {/* Hero Content */}
-      <div className="h-full flex justify-between items-end max-w-[1440px] mx-auto pb-[40px] relative z-10 px-[25px]">
+      <div className="h-full flex justify-between items-end max-w-[1440px] mx-auto w-full px-[25px]">
         {/* Top left icon */}
 
         <motion.div
@@ -45,7 +46,7 @@ const Hero = () => {
           className={cn(
             "absolute ",
             "lg:left-[300px] sm:left-[100px] left-6",
-            "md:top-1/4 top-[25%]"
+            "md:top-[20%] top-[25%]"
           )}
         >
           <IconBox img="/images/icon1.svg" rotate="-42deg" />
@@ -59,7 +60,7 @@ const Hero = () => {
           className={cn(
             "absolute ",
             "lg:right-[300px] sm:right-[100px] right-6",
-            "md:top-1/4 top-[25%]"
+            "md:top-[20%] top-[25%]"
           )}
         >
           <IconBox img="/images/icon2.svg" rotate="42deg" />
@@ -73,7 +74,7 @@ const Hero = () => {
           className={cn(
             "absolute ",
             "lg:left-[300px] sm:left-[100px] left-6",
-            "md:top-[60%] top-[60%]"
+            "md:top-[65%] top-[60%]"
           )}
         >
           <IconBox img="/images/icon3.svg" rotate="-60deg" />
@@ -87,7 +88,7 @@ const Hero = () => {
           className={cn(
             "absolute ",
             "lg:right-[300px] sm:right-[100px] right-6",
-            "md:top-[60%] top-[60%]"
+            "md:top-[65%] top-[60%]"
           )}
         >
           <IconBox img="/images/icon4.svg" rotate="60deg" />
@@ -99,14 +100,14 @@ const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 2.3 }}
           className={cn(
-            "w-full h-full",
-            "md:flex-row flex flex-col  ",
-            " md:items-end md:justify-between items-center justify-center ",
-            "gap-6 md:gap-4"
+            "w-full h-full z-10",
+            " flex flex-col  ",
+            " items-center justify-center ",
+            "gap-6 md:gap-8"
           )}
         >
-          <div className="md:text-[#ADD030] text-white md:text-left text-center">
-            <h1 className="md:text-[48px] text-[32px] font-medium md:font-semibold leading-[1] tracking-tighter ">
+          <div className=" text-white  text-center">
+            <h1 className="md:text-[60px] text-[32px] font-medium md:font-semibold leading-[1] tracking-tighter ">
               Build Strength.
               <br />
               Unlock Your Potential.
