d6bc0da7a12db605e62def6f2672d640ed1d9460
amarnath666 <amarnathdhumal2001@gmail.com>
2025-11-25 21:13:57 +0530

    refactor: add cn import to carousel component

--- app/components/carousel/coursel.tsx
+++ app/components/carousel/coursel.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import { cn } from "@/lib/utils";
 
+
 interface CourselProps {
     images?: string[];
     className?: string;
