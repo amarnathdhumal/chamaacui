ebfd4a40077a2883b076f00e359f20c1e33e1037
amarnath666 <amarnathdhumal2001@gmail.com>
2025-11-25 21:35:36 +0530

    refactor: wrap RandomImageReveal in centered container

--- app/components/random-image-reveal/reveal-card-demo.tsx
+++ app/components/random-image-reveal/reveal-card-demo.tsx
@@ -2,10 +2,12 @@ import RandomImageReveal from "./reveal-card";
 
 export default function RandomImageRevealDemo() {
     return (
-        <RandomImageReveal
-            images={["/images/1.jpg", "/images/2.jpg", "/images/3.jpg", "/images/4.jpg", "/images/5.jpg"]}
-            duration={0.2}
-        />
+        <div className="w-full h-[600px] flex justify-center items-center">
+            <RandomImageReveal
+                images={["/images/1.jpg", "/images/2.jpg", "/images/3.jpg", "/images/4.jpg", "/images/5.jpg"]}
+                duration={0.2}
+            />
+        </div>
     );
 }
 
