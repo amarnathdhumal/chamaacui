cf704d810a9d26aec233d38388aaf0a1616a2d3d
amarnath666 <amarnathdhumal2001@gmail.com>
2025-12-28 04:50:00 +0530

    fix: ui alginment in sidebar

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
   }, {} as Record<string, typeof sidebarData>);
 
   return (
-    <div className="min-w-45 max-w-45 h-[calc(100vh-120px)] flex flex-col sticky top-[114px] overflow-y-auto hide-scrollbar pb-10 pl-3">
+    <div className="min-w-45 max-w-45 h-[calc(100vh-120px)] flex flex-col sticky top-[114px] overflow-y-auto hide-scrollbar pb-10 ">
       <ul className="flex flex-col gap-4">
         {Object.entries(groupedData).map(([category, items]) => (
           <li key={category} className="flex flex-col gap-4">
@@ -45,35 +45,38 @@ const Sidebar = () => {
                 const isHovered = hoveredItem === itemId;
 
                 return (
-                  <li key={itemId}>
+                  <li
+                    key={itemId}
+                    className="relative pl-3"
+                    onMouseEnter={() => setHoveredItem(itemId)}
+                    onMouseLeave={() => setHoveredItem(null)}
+                  >
+                    <div className="absolute left-0 top-1/2 -translate-y-1/2 w-[5px] h-[5px] flex items-center justify-center">
+                      {isActive && (
+                        <div className="absolute inset-0 m-auto w-[5px] h-[5px] rounded-full bg-black dark:bg-white" />
+                      )}
+                      {isHovered && (
+                        <motion.div
+                          layoutId="hover-dot"
+                          className="absolute inset-0 m-auto w-[5px] h-[5px] rounded-full bg-black dark:bg-white"
+                          transition={{
+                            type: "spring",
+                            stiffness: 300,
+                            damping: 30,
+                          }}
+                        />
+                      )}
+                    </div>
                     <motion.a
                       href={href}
                       className={cn(
-                        " cursor-pointer font-normal text-sm/5  flex flex-row items-center relative  hover:text-black dark:hover:text-white hover:font-medium",
-                        `${isActive ? "text-black dark:text-white  font-medium" : "text-gray-600 dark:text-gray-400 "}`
+                        "cursor-pointer font-normal text-sm/5 flex flex-row items-center hover:text-black dark:hover:text-white hover:font-medium",
+                        `${isActive ? "text-black dark:text-white font-medium" : "text-gray-600 dark:text-gray-400"}`
                       )}
-                      onMouseEnter={() => setHoveredItem(itemId)}
-                      onMouseLeave={() => setHoveredItem(null)}
                     >
-                      <div className="absolute left-[-12px] top-1/2 -translate-y-1/2 w-[5px] h-[5px] flex items-center justify-center">
-                        {isActive && (
-                          <div className="absolute inset-0 m-auto w-[5px] h-[5px] rounded-full bg-black dark:bg-white" />
-                        )}
-                        {isHovered && (
-                          <motion.div
-                            layoutId="hover-dot"
-                            className="absolute inset-0 m-auto w-[5px] h-[5px] rounded-full bg-black dark:bg-white"
-                            transition={{
-                              type: "spring",
-                              stiffness: 300,
-                              damping: 30,
-                            }}
-                          />
-                        )}
-                      </div>
                       {item.componentName}
                       {item.isNew && (
-                        <span className="ml-2 text-[10px] bg-[#adfa1d]    text-black px-1.5 py-1 rounded-[8px] leading-none">
+                        <span className="ml-2 text-[10px] bg-[#adfa1d] text-black px-1.5 py-1 rounded-[8px] leading-none">
                           New
                         </span>
                       )}
