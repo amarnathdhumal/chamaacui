db68bb7a239215709e83c8104178f9b7d553a7f4
amarnath666 <amarnathdhumal2001@gmail.com>
2026-01-30 07:11:33 +0530

    refactor: changed grid-4 to grid-3

--- app/components/buttons/buttons-grid.tsx
+++ app/components/buttons/buttons-grid.tsx
@@ -94,7 +94,7 @@ export default function ButtonsGrid() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {buttons.map((button) => (
           <motion.div
             key={button.name}
