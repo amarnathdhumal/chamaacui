1c07c5eaabde9182a3250489b0b7f3287ea07654
amarnath666 <amarnathdhumal2001@gmail.com>
2026-02-06 22:04:17 +0530

    chore: add common non-existent page paths to robots meta

--- app/robots.ts
+++ app/robots.ts
@@ -7,12 +7,21 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: [
         "/private/",
+        // RSS/Feed paths that don't exist
         "/rss.rss",
         "/rss.xml",
         "/feed.rss",
         "/feed.xml",
         "/registry/feed.xml",
-        "/ingest/", // Block PostHog proxy path (deprecated)
+        "/ingest/", // Deprecated PostHog proxy
+        // Common pages bots probe that don't exist
+        "/contact",
+        "/about",
+        "/home",
+        "/admin",
+        "/login",
+        "/wp-admin",
+        "/wp-login",
       ],
     },
     sitemap: "https://www.chamaac.com/sitemap.xml",
